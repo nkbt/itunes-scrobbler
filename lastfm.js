056f35d0177f8a8501f3e4745e86753f3a2613e6
Nik Butenko <nik@butenko.me>
2017-02-11 22:52:34 +1100

    Create custom LastFM client

---
+++ lastfm.js
@@ -0,0 +1,99 @@
+'use strict';
+
+
+const crypto = require('crypto');
+const querystring = require('querystring');
+const fetch = require('node-fetch');
+
+
+const LASTFM_API = 'http://ws.audioscrobbler.com/2.0';
+
+
+const makeHash = input =>
+  crypto.createHash('md5').update(input, 'utf8').digest('hex');
+
+
+const post = body => fetch(LASTFM_API, {method: 'POST', body})
+  .then(res => res.json())
+  .then(json => {
+    console.log(JSON.stringify(json, null, 2));
+  });
+
+
+const LastFm = function ({LASTFM_API_KEY, LASTFM_SECRET, LASTFM_USER, LASTFM_PASSWORD}) {
+  this.apiKey = LASTFM_API_KEY;
+  this.apiSecret = LASTFM_SECRET;
+  this.username = LASTFM_USER;
+  this.password = LASTFM_PASSWORD;
+  this.sessionKey = null
+};
+
+
+LastFm.prototype.love = function ({artist, album = '_', track}) {
+  return this.prepareData({method: 'track.love', artist, album, track})
+    .then(post);
+};
+
+
+LastFm.prototype.unlove = function ({artist, album = '_', track}) {
+  return this.prepareData({method: 'track.unlove', artist, album, track})
+    .then(post);
+};
+
+
+LastFm.prototype.scrobble = function ({artist, album = '_', track, lastPlayed}) {
+  const timestamp = Math.floor(lastPlayed > 0 ? lastPlayed : Date.now() / 1000);
+
+  return this.prepareData({method: 'track.scrobble', artist, album, track, timestamp})
+    .then(post)
+};
+
+
+LastFm.prototype.makeSig = function (params) {
+  return makeHash(Object.keys(params).sort()
+    .reduce((result, key) =>
+      `${params[key]}`.length > 0 ?
+        result.concat(key).concat(`${params[key]}`) :
+        result, '')
+    .concat(this.apiSecret));
+};
+
+
+LastFm.prototype.getSessionKey = function () {
+  if (this.sessionKey) {
+    return Promise.resolve(this.sessionKey);
+  }
+
+  const params = {
+    method: 'auth.getMobileSession',
+    username: this.username,
+    authToken: makeHash(`${this.username}${makeHash(this.password)}`),
+    api_key: this.apiKey
+  };
+  const data = querystring.stringify(Object.assign(params, {
+    api_sig: this.makeSig(params),
+    format: 'json'
+  }));
+
+  return fetch(`${LASTFM_API}?${data}`)
+    .then(res => res.json())
+    .then(res => {
+      this.sessionKey = res.session.key;
+      return this.sessionKey;
+    });
+};
+
+
+LastFm.prototype.prepareData = function (params) {
+  return this.getSessionKey()
+    .then(sk => {
+      const paramsWithAuth = Object.assign({api_key: this.apiKey, sk}, params);
+      return querystring.stringify(Object.assign(paramsWithAuth, {
+        api_sig: this.makeSig(paramsWithAuth),
+        format: 'json'
+      }))
+    });
+};
+
+
+module.exports = LastFm;
