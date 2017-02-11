3f4ad61949dd75575f278b4c4422114429683a34
Nik Butenko <nik@butenko.me>
2017-02-12 02:50:19 +1100

    Load config and database from current folder

--- db.js
+++ db.js
@@ -3,9 +3,11 @@
 
 const levelup = require('levelup');
 const Promise = require('bluebird');
+const path = require('path');
 
 
-exports.db = Promise.promisifyAll(levelup('./itunes', {
+
+exports.db = Promise.promisifyAll(levelup(path.join(process.cwd(), 'itunes'), {
   keyEncoding: 'json',
   valueEncoding: 'json'
 }));
--- scrobbler.js
+++ scrobbler.js
@@ -1,15 +1,17 @@
 'use strict';
 
 
+const path = require('path');
 const LastFm = require('./lastfm');
 
 
 const options = {};
 try {
-  Object.assign(options, require('./secret.json'));
+  Object.assign(options, require(path.join(process.cwd(), 'secret.json')));
 } catch (err) {
   Object.assign(options, process.env);
 }
+
 
 const {
   LASTFM_API_KEY,
