17601541421046472a5732ad823f1b2f79c5ec76
Nik Butenko <nik@butenko.me>
2017-02-12 01:03:45 +1100

    Make promise queue great again

--- promiseQueue.js
+++ promiseQueue.js
@@ -4,6 +4,19 @@
 const Promise = require('bluebird');
 
 
-exports.promiseQueue = (items, processor) => items
-  .reduce((promise, track) =>
-    promise.then(() => processor(track)), Promise.resolve());
+const noop = () => {};
+
+
+exports.promiseQueue = (items, processor, tap = noop) => {
+  const results = [];
+
+  return items
+    .reduce(
+      (promise, item, i) =>
+        promise.then(() =>
+          processor(item, i)
+            .tap(result => tap(result, item, i))
+            .then(result => results.push(result))),
+      Promise.resolve())
+    .then(() => results);
+};
